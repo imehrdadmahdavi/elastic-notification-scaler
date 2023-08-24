f72e0e9881e812a354c1f6e422d3838a66c3ee39
Mehrdad Mahdavi <imehrdadmahdavi@gmail.com>
2023-08-24 15:30:01 -0700

    control plane can now relized the exact status of table and worker pods while scaling up and down

--- images/control-plane/main.go
+++ images/control-plane/main.go
@@ -28,6 +28,53 @@ func setupDatabase() {
 	if err != nil {
 		log.Fatalf("Failed to connect to the database: %v", err)
 	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatalf("Failed to ping the database: %v", err)
+	}
+
+	// Check if table 'work_items' exists
+	var tableName string
+	err = db.QueryRow("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'work_items'").Scan(&tableName)
+	if err != nil && err != sql.ErrNoRows {
+		log.Fatalf("Failed to check for existing table: %v", err)
+	}
+
+	// Drop table if it exists
+	if tableName == "work_items" {
+		_, err = db.Exec("DROP TABLE work_items")
+		if err != nil {
+			log.Fatalf("Failed to drop existing table: %v", err)
+		}
+	}
+
+	_, err = db.Exec("CREATE EXTENSION IF NOT EXISTS \"pgcrypto\";")
+	if err != nil {
+		log.Fatalf("Failed to enable pgcrypto extension: %v", err)
+	}
+
+	// Create new table 'work_items' with updated schema
+	_, err = db.Exec(`
+        CREATE TABLE work_items (
+			id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+			value INTEGER DEFAULT 0,
+			currentWorker VARCHAR(255) DEFAULT NULL
+        )
+    `)
+	if err != nil {
+		log.Fatalf("Failed to create new table: %v", err)
+	}
+
+	// Insert sample data
+	for i := 1; i <= 5; i++ {
+		// Inserting with default values for UUID and currentWorker, and value set to 0
+		_, err := db.Exec("INSERT INTO work_items (value) VALUES (DEFAULT)")
+		if err != nil {
+			log.Printf("Failed to insert record %d: %v", i, err)
+		} else {
+			log.Printf("Successfully inserted record %d", i)
+		}
+	}
 }
 
 func connectToRedis() *redis.Client {
@@ -57,10 +104,48 @@ func printInfo(rdb *redis.Client) {
 		}
 
 		// Fetch the number of worker pods from Redis
-		workerCount := rdb.XLen(ctx, "worker-status").Val()
+		workerCount := rdb.SCard(ctx, "workers").Val() // Use the set to get the count of workers
 
 		log.Printf("Records in work_items table: %d", count)
 		log.Printf("Number of running worker pods: %d", workerCount)
+	}
+}
+
+func sendIDsToRedis(rdb *redis.Client) {
+	// 1. Query all IDs from the work_items table
+	rows, err := db.Query("SELECT id FROM work_items")
+	if err != nil {
+		log.Printf("Failed to fetch IDs from the database: %v", err)
+		return
+	}
+	defer rows.Close()
+
+	var ids []int
+	for rows.Next() {
+		var id int
+		if err := rows.Scan(&id); err != nil {
+			log.Printf("Failed to scan ID from the row: %v", err)
+			return
+		}
+		ids = append(ids, id)
+	}
+
+	// Convert the list of IDs to a comma-separated string for easier handling
+	idStr := fmt.Sprintf("[%v]", ids)
+
+	// 2. Iterate over the worker entries in the Redis set
+	workers := rdb.SMembers(ctx, "workers").Val()
+	for _, worker := range workers {
+		// 3. Update the list of IDs for each worker in the Redis stream
+		_, err = rdb.XAdd(ctx, &redis.XAddArgs{
+			Stream: "worker-status",
+			Values: map[string]interface{}{"name": worker, "ids": idStr},
+		}).Result()
+
+		if err != nil {
+			log.Printf("Failed to update IDs for worker %s in Redis: %v", worker, err)
+			continue
+		}
 	}
 }
 
@@ -78,9 +163,13 @@ func main() {
 	log.Println("Starting control-plane application...")
 	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/ready", readinessHandler)
+
 	setupDatabase()
 
 	rdb := connectToRedis()
+
+	// I commented out this line since the function isn't defined in the provided code.
+	// sendIDsToRedis(rdb)
 
 	go printInfo(rdb)
 
--- images/worker/main.go
+++ images/worker/main.go
@@ -1,17 +1,60 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/go-redis/redis/v8"
+	_ "github.com/lib/pq"
 	"golang.org/x/net/context"
 )
 
 var ctx = context.Background()
 var rdb *redis.Client
+var db *sql.DB
+
+func setupDatabase() {
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"))
+
+	var err error
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+}
+
+func registerWorkerToRedis(rdb *redis.Client) {
+	podName := os.Getenv("POD_NAME")
+
+	// Check if the worker is already registered
+	exists := rdb.SIsMember(ctx, "workers", podName).Val()
+	if !exists {
+		// Add worker name to the set
+		rdb.SAdd(ctx, "workers", podName)
+
+		// Register worker in the stream with status RUNNING and no IDs
+		rdb.XAdd(ctx, &redis.XAddArgs{
+			Stream: "worker-status",
+			Values: map[string]interface{}{"name": podName, "status": "RUNNING", "ids": "[]"},
+		})
+	}
+}
+
+func deregisterWorkerFromRedis(rdb *redis.Client) {
+	podName := os.Getenv("POD_NAME")
+	// Remove the worker name from the Redis set
+	rdb.SRem(ctx, "workers", podName)
+	log.Printf("Deregistered worker: %s", podName)
+}
 
 func connectToRedis() {
 	rdb = redis.NewClient(&redis.Options{
@@ -36,22 +79,57 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Ready!")
 }
 
+func updateDatabase() {
+	podName := os.Getenv("POD_NAME")
+
+	// Infinite loop to listen to new messages from Redis stream
+	for {
+		xreadResp, err := rdb.XRead(ctx, &redis.XReadArgs{
+			Streams: []string{"worker-status", "0"},
+			Count:   10,
+		}).Result()
+		if err != nil {
+			log.Printf("Failed to read from Redis stream: %v", err)
+			continue
+		}
+
+		for _, xMessage := range xreadResp[0].Messages {
+			for _, xValue := range xMessage.Values {
+				if id, ok := xValue.(int); ok {
+					// Update the database record with the given ID
+					_, err := db.Exec("UPDATE work_items SET value = value + 1, currentworker = $1 WHERE id = $2", podName, id)
+					if err != nil {
+						log.Printf("Failed to update database record with ID %d: %v", id, err)
+					}
+				}
+			}
+		}
+	}
+}
+
 func main() {
 	log.Println("Starting worker application...")
 	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/ready", readinessHandler)
 
+	setupDatabase()
 	connectToRedis()
+	registerWorkerToRedis(rdb)
 
-	// Register worker status in Redis
-	podName := os.Getenv("POD_NAME")
-	_, err := rdb.XAdd(ctx, &redis.XAddArgs{
-		Stream: "worker-status",
-		Values: map[string]interface{}{"message": fmt.Sprintf("STARTED:%s", podName)},
-	}).Result()
-	if err != nil {
-		log.Fatalf("Failed to send worker status to Redis: %v", err)
-	}
+	// Create a channel to listen for system signals
+	sigChan := make(chan os.Signal, 1)
+	// Register for SIGINT (Ctrl+C) and SIGTERM (termination by system) signals
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start a goroutine that listens on sigChan
+	go func() {
+		sig := <-sigChan // Block until a signal is received
+		log.Printf("Received signal: %v. Deregistering worker...", sig)
+		deregisterWorkerFromRedis(rdb)
+		os.Exit(0)
+	}()
+
+	go updateDatabase()
 
 	http.ListenAndServe(":8080", nil)
 }
